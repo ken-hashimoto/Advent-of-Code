06ea4c5f023056fc5d0cb5c926b2043ae6ea3449
hashimoto-k <hashimoto.kenji.1102@gmail.com>
2022-12-08 23:55:51 +0900

    Day 1

---
+++ Day1/main.py
@@ -0,0 +1,11 @@
+PATH = "Day1\input.txt"
+
+with open(PATH,encoding="UTF-8") as input_file:
+  text = input_file.read().split("\n\n")
+  calories_per_elf = [list(map(int, line.splitlines())) for line in text]
+  total = [sum(calorie) for calorie in calories_per_elf]
+  # part1
+  print(max(total))
+  # part2
+  total.sort(reverse=True)
+  print(sum(total[:3]))
